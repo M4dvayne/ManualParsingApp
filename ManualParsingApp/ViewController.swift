d911f775188a2114a71a3ebd3839e3b354b0a634
KremnevViacheslav <rusted2192@gmail.com>
2022-03-08 19:21:31 +0300

    Initial Commit

---
+++ ManualParsingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManualParsingApp
+//
+//  Created by Вячеслав Кремнев on 08.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
